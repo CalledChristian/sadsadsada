183b03cb4d2c6b6ae80f09180b9f041f6949c9ec
Carlos <cpisco@pucp.edu.pe>
2023-06-20 19:12:16 -0500

    commit 1

---
+++ src/main/java/com/example/vfssd/VfssdApplication.java
@@ -0,0 +1,13 @@
+package com.example.vfssd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfssdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfssdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vfssd/VfssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vfssd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfssdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
